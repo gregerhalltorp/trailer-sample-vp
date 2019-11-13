28c74c6949cde31399b12809951409181fb12aef
Greger Hälltorp <greger.halltorp@gmail.com>
2019-11-14 00:31:20 +0100

    build working

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/server.js',
@@ -8,7 +10,11 @@ export default {
     format: 'cjs',
   },
   plugins: [
-    resolve(),
+    resolve({
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    json(),
     babel({
       exclude: 'node_modules/**',
     }),
